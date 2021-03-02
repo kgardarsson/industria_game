b49c1cd0a2b6111b9a0a2a07a39a247b6e919cce
Kristinn Bragi <kristinnbragi@gmail.com>
2021-03-02 12:09:16 +0100

    Energy System Initial

---
+++ Industria TaPoGaDaD Game/Assets/_Project/2 Scripts/EnergyContainer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyContainer : MonoBehaviour {
+
+    public uint energyItTakes;
+    public bool on;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        // if (Input.anyKeyDown) {
+        //     interact();
+        // }
+    }
+
+    public void interact() {
+        EnergyManager em = FindObjectOfType<EnergyManager>();
+        if (on) {
+            em.energy += this.energyItTakes;
+            on = !on;
+        } else {
+            if(em.energy >= this.energyItTakes) {
+                em.energy -= this.energyItTakes;
+                on = !on;
+            }
+        }
+    }
+}
