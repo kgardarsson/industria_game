5ac1a4559cf4dfebe321b89354d942adb28c0205
malteerasmussen <malteerasmussen@gmail.com>
2021-03-02 14:42:17 +0100

    something

---
+++ Industria TaPoGaDaD Game/Assets/_Project/1 Scenes/Malte/ButtonController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject controlledObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pressPossible(){
+        if (Input.GetKeyDown("space"))
+        {
+
+            if (this.tag == "DoorButton") {
+                controlledObject.SetActive(!controlledObject.activeInHierarchy);
+            }
+            if (this.tag == "LightButton") {
+                controlledObject.SetActive(!controlledObject.activeInHierarchy);
+            }
+        }
+    }
+}
