0d289573dd5b06e18dbd95e7c8949abc6859f782
Tor arnth  Petersen <torarnthpetersen@MacBook-Pro.local>
2021-03-07 20:42:00 +0100

    fixed door animation. Animation-script is added to the door prefab and the interaction is added to the buttons.

---
+++ Industria TaPoGaDaD Game/Assets/_Project/2 Scripts/DoorTriggerButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTriggerButton : MonoBehaviour
+{
+   [SerializeField] public DoorAnimated door1;
+
+
+   private void Update(){
+
+       if (Input.GetKeyDown(KeyCode.F)){
+           door1.OpenDoor();
+           print("yesyesyesye");
+       }
+
+        if (Input.GetKeyDown(KeyCode.G)){
+            door1.CloseDoor();
+            print("nonono");
+
+       }
+   }
+
+
+}
