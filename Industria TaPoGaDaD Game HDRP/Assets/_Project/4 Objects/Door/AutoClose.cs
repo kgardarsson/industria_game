1492fec09b42c9eb51a9234de559f5ccb78cfea8
Kristinn Bragi <kristinnbragi@gmail.com>
2021-03-16 13:35:33 +0100

    Finish Door Prefab

---
+++ Industria TaPoGaDaD Game HDRP/Assets/_Project/4 Objects/Door/AutoClose.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoClose : MonoBehaviour {
+    public Collider Player;
+
+    private void OnTriggerEnter(Collider Player) {
+        if (transform.parent.TryGetComponent<DoorController>(out DoorController door)) {
+            door.Open(false);
+        } else {
+            Debug.LogError("GameObject of AutoClose component needs to be a child of GameObject with DoorController");
+        }
+    }
+}
