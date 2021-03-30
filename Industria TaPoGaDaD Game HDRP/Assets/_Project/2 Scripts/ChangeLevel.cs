eeed2e935f5ef3fa6025d63de9633319c944717a
malteerasmussen <malteerasmussen@gmail.com>
2021-03-30 10:49:00 +0200

    assets and nextLevel script

---
+++ Industria TaPoGaDaD Game HDRP/Assets/_Project/2 Scripts/ChangeLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    public Object nextScene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(nextScene.name.ToString());
+    }
+}
