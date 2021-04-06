310924f399d24bd34d21b6e94e0374273a7a5d65
Tor Arnth  Petersen <torarnthpetersen@MacBook-Pro-tilhrende-Tor.local>
2021-04-06 14:31:52 +0200

    alarm on panel-button

---
+++ Industria TaPoGaDaD Game HDRP/Assets/_Project/2 Scripts/Alarm.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    public AudioSource playaudio;  
+ 
+    public bool playing = true;
+    public bool failSafe = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            playaudio = GetComponent<AudioSource>();
+
+    }
+
+
+    void OnMouseDown() {
+        if (playing == false)
+        {
+        playaudio.Play();
+        playing = true; 
+        failSafe = true;
+        print(playing);
+        StartCoroutine(FailSafe(0.25f));
+        }
+
+
+        else if (playing == true)
+        {
+
+        failSafe =true; 
+        playaudio.Stop();
+        playing = false;
+        print(playing);
+
+        StartCoroutine(FailSafe(0.25f));
+
+
+        }
+    }
+
+    IEnumerator FailSafe(float waitTime){
+        yield return new WaitForSeconds(waitTime);
+        failSafe =false;
+    }
+    
+}
