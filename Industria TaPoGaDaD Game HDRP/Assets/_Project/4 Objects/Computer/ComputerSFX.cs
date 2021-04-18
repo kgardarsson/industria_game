6302876c2343b1cbb57beec28b7302aac9974762
Kristinn Bragi <kristinnbragi@gmail.com>
2021-04-18 19:32:22 +0200

    Computer buzz

---
+++ Industria TaPoGaDaD Game HDRP/Assets/_Project/4 Objects/Computer/ComputerSFX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerSFX : MonoBehaviour {
+
+    public AudioClip start;
+    public AudioClip running;
+    public AudioClip end;
+    private AudioSource src;
+
+    void Awake() {
+        src = GetComponent<AudioSource>();
+        playAudio();
+    }
+
+    public void playAudio() {
+        src.clip = start;
+        src.loop = true;
+        src.Play();
+        Invoke("setRunning", start.length);
+    }
+
+    private void setRunning() {
+        src.clip = running;
+        src.Play();
+    }
+
+    public void stopAudio() {
+        CancelInvoke();
+        src.clip = end;
+        src.loop = false;
+        src.Play();
+    }
+}
